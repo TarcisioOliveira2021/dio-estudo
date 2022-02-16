5d7250805b410f882de04e58a3c9750f77f2f3a2
TarcisioOliveira2021 <tarcisio.zark.veloso@gmail.com>
2022-02-16 00:03:44 -0300

    Subindo as atividade solicitadas na aula.

---
+++ EstudoMetodos/src/com/me/dio/Calculadora.java
@@ -0,0 +1,24 @@
+package com.me.dio;
+
+public class Calculadora {
+
+	public static void soma(double x,double y) {
+		double res = x + y;
+		System.out.println("A soma dos numeros: "+x+"+"+y+" é "+res);
+	}
+	
+	public static void subtrair(double x,double y) {
+		double res = x + y;
+		System.out.println("A subtração dos numeros: "+x+"+"+y+" é "+res);
+	}
+	
+	public static void soma(double x,double y) {
+		double res = x + y;
+		System.out.println("A soma dos numeros: "+x+"+"+y+" é "+res);
+	}
+	
+	public static void soma(double x,double y) {
+		double res = x + y;
+		System.out.println("A soma dos numeros: "+x+"+"+y+" é "+res);
+	}
+}
