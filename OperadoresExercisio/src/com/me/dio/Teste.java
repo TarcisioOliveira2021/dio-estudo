8dbdb50482ebfb9bfe757ea60a813d6f8e9d6b95
Tarcisio <tarcisio.zark.veloso@gmail.com>
2022-02-22 16:52:32 -0300

    Subindo exercisio referente a operadores Java
    - Subinnndooo!!!

---
+++ OperadoresExercisio/src/com/me/dio/Teste.java
@@ -0,0 +1,21 @@
+package com.me.dio;
+/*
+ * @author: Tarcisio de Oliveira
+ */
+public class Teste {
+	//Curiosidade que eu não sabia:
+	public static void main(String[] args) {
+		char c1 = 'a';
+		char c2 = 'b';
+		char c3 = 'c';
+		char c4 = 'd';
+		char c5 = 'd';
+		System.out.println("c1 é maior que c2 ? : "+(c1 > c2));
+		System.out.println("c2 é maior que c1 ? : "+(c2 > c1));
+		System.out.println("c3 é maior que c4 ? : "+(c3 > c4));
+		System.out.println("c4 é maior que c3 ? : "+(c4 > c3));
+		System.out.println("c4 é maior ou igual a c5 ? : "+(c4 >= c5));
+
+	}
+
+}
