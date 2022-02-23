5a2bcb6d32c4df526a51f301f37d8ce19347d5d7
Tarcisio <tarcisio.zark.veloso@gmail.com>
2022-02-23 15:37:53 -0300

    Subindo um teste de como criar um Array de string fácilmente
    - Subindo !

---
+++ Teste/src/com/ucsal/teste/Teste.java
@@ -0,0 +1,14 @@
+package com.ucsal.teste;
+
+import java.util.Iterator;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		String nomes[]= {"Joao", "Maria"};
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.print(nomes[i]+" ");
+		}
+	}
+
+}
